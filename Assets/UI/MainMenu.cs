731bb3d190126bdbed7b78e9ea45dee6982a7967
Wolfgang Groetz <wolfganggroetz@gmail.com>
2025-01-25 18:24:02 -0500

    Fixed stuff and things

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -37,6 +38,7 @@ public class MainMenu : MonoBehaviour
         
         _creditsXButton = _ui.Q<Button>("CreditsX");
         _creditsXButton.clicked += OnCreditsClosed;
+        _creditsButton.RegisterCallback<TransitionEndEvent>(OnCreditsPopTransitionEnd);
         
         _quitButton = _ui.Q<Button>("QuitButton");
         _quitButton.clicked += OnQuitButtonClicked;
@@ -60,6 +62,7 @@ public class MainMenu : MonoBehaviour
         properties.Add(new StylePropertyName("scale"));
         properties.Add(new StylePropertyName("opacity"));
         properties.Add(new StylePropertyName("visibility"));
+
         button.style.transitionProperty = new StyleList<StylePropertyName>(properties);
         
         List<TimeValue> durations = new List<TimeValue>();
@@ -73,6 +76,7 @@ public class MainMenu : MonoBehaviour
         button.style.scale = new StyleScale(new Vector2(1.3f, 1.3f));
         button.style.opacity = 0;
         button.style.visibility = new StyleEnum<Visibility>(Visibility.Hidden);
+
     }
 
     private void PlayButtonPopAnimation(Button button)
@@ -97,17 +101,11 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Credits Button clicked");
 
-        _title.style.display = DisplayStyle.None;
-        _playButton.style.display = DisplayStyle.None;
-        _optionsButton.style.display = DisplayStyle.None;
-        _quitButton.style.display = DisplayStyle.None;
-
         _creditsButton.text = "Main Dev Team:\nAaron Radliff\nJeremiah Opare\nRae Benkovich\nWolfgang Groetz\n\nMusic:\nGarrison Bouchard-Ferdon\n\nFont:\nDynaPuff Font from Google Fonts";
         
         List<StylePropertyName> properties = new List<StylePropertyName>();
         properties.Add(new StylePropertyName("width"));
         properties.Add(new StylePropertyName("height"));
-        properties.Add(new StylePropertyName("text"));
         _creditsButton.style.transitionProperty = new StyleList<StylePropertyName>(properties);
 
         List<TimeValue> durations = new List<TimeValue>();
@@ -115,95 +113,55 @@ public class MainMenu : MonoBehaviour
         _creditsButton.style.transitionDuration = new StyleList<TimeValue>(durations);
 
         List<EasingFunction> easingFunctions = new List<EasingFunction>();
-        easingFunctions.Add(new EasingFunction(EasingMode.Ease));
+        easingFunctions.Add(new EasingFunction(EasingMode.EaseIn));
         _creditsButton.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
 
         _creditsButton.style.width = new StyleLength(new Length(90.0f, LengthUnit.Percent));
         _creditsButton.style.height = new StyleLength(new Length(90.0f, LengthUnit.Percent));
         
-        List<StylePropertyName> properties2 = new List<StylePropertyName>();
-        properties2.Add(new StylePropertyName("display"));
-        _creditsXButton.style.transitionProperty = new StyleList<StylePropertyName>(properties2);
-        _creditsXButton.style.transitionDuration = new StyleList<TimeValue>(durations);
-        _creditsXButton.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
         _creditsXButton.style.display = DisplayStyle.Flex;
-        
-        List<TimeValue> durations2 = new List<TimeValue>();
-        durations2.Add(new TimeValue(buttonDisableDuration, TimeUnit.Second));
-        
-        _title.style.transitionProperty  = new StyleList<StylePropertyName>(properties2);
-        _title.style.transitionDuration = new StyleList<TimeValue>(durations2);
-        _title.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
-        _title.style.display = DisplayStyle.None;
-        
-        _playButton.style.transitionProperty  = new StyleList<StylePropertyName>(properties2);
-        _playButton.style.transitionDuration = new StyleList<TimeValue>(durations2);
-        _playButton.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
-        _playButton.style.display = DisplayStyle.None;
-        
-        _optionsButton.style.transitionProperty  = new StyleList<StylePropertyName>(properties2);
-        _optionsButton.style.transitionDuration = new StyleList<TimeValue>(durations2);
-        _optionsButton.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
-        _optionsButton.style.display = DisplayStyle.None;
-        
-        _quitButton.style.transitionProperty  = new StyleList<StylePropertyName>(properties2);
-        _quitButton.style.transitionDuration = new StyleList<TimeValue>(durations2);
-        _quitButton.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
-        _quitButton.style.display = DisplayStyle.None;
+        SetVisibility(DisplayStyle.None, new VisualElement[]{_title, _playButton, _optionsButton, _quitButton});
     }
 
     private void OnCreditsClosed()
     {
         Debug.Log("Credits Closed");
+        
+        ButtonPopAnimation(_creditsButton);
+    }
 
-        _creditsButton.text = "Credits";
-        
-        List<StylePropertyName> properties = new List<StylePropertyName>();
-        properties.Add(new StylePropertyName("scale"));
-        properties.Add(new StylePropertyName("width"));
-        properties.Add(new StylePropertyName("height"));
-        _creditsButton.style.transitionProperty = new StyleList<StylePropertyName>(properties);
+    private void SetVisibility(DisplayStyle displayStyle, VisualElement[] elements)
+    {
+        foreach (VisualElement element in elements)
+        {
+            element.style.display = displayStyle;
+        }
+    }
 
-        List<TimeValue> durations = new List<TimeValue>();
-        durations.Add(new TimeValue(0.5f, TimeUnit.Second));
-        _creditsButton.style.transitionDuration = new StyleList<TimeValue>(durations);
+    private void OnCreditsPopTransitionEnd(TransitionEndEvent endEvent)
+    {
+        if (endEvent.target == _creditsButton && endEvent.stylePropertyNames.Contains("opacity") && _creditsButton.style.opacity.value < 1.0f)
+        {
+            Debug.Log(endEvent.target.ToString());
+            _creditsButton.text = "Credits";
+            
+            List<TimeValue> durations = new List<TimeValue>();
+            durations.Add(new TimeValue(0.0f, TimeUnit.Second));
+            _creditsButton.style.transitionDuration = new StyleList<TimeValue>(durations);
 
-        List<EasingFunction> easingFunctions = new List<EasingFunction>();
-        easingFunctions.Add(new EasingFunction(EasingMode.Ease));
-        _creditsButton.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
-
-        _creditsButton.style.width = new StyleLength(new Length(20.0f, LengthUnit.Percent));
-        _creditsButton.style.height = new StyleLength(new Length(12.0f, LengthUnit.Percent));
-        
-        List<StylePropertyName> properties2 = new List<StylePropertyName>();
-        properties2.Add(new StylePropertyName("display"));
-        _creditsXButton.style.transitionProperty = new StyleList<StylePropertyName>(properties2);
-        _creditsXButton.style.transitionDuration = new StyleList<TimeValue>(durations);
-        _creditsXButton.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
-        _creditsXButton.style.display = DisplayStyle.None;
-        
-        List<TimeValue> durations2 = new List<TimeValue>();
-        durations2.Add(new TimeValue(buttonDisableDuration, TimeUnit.Second));
-        
-        _title.style.transitionProperty  = new StyleList<StylePropertyName>(properties2);
-        _title.style.transitionDuration = new StyleList<TimeValue>(durations2);
-        _title.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
-        _title.style.display = DisplayStyle.Flex;
-        
-        _playButton.style.transitionProperty  = new StyleList<StylePropertyName>(properties2);
-        _playButton.style.transitionDuration = new StyleList<TimeValue>(durations2);
-        _playButton.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
-        _playButton.style.display = DisplayStyle.Flex;
-        
-        _optionsButton.style.transitionProperty  = new StyleList<StylePropertyName>(properties2);
-        _optionsButton.style.transitionDuration = new StyleList<TimeValue>(durations2);
-        _optionsButton.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
-        _optionsButton.style.display = DisplayStyle.Flex;
-        
-        _quitButton.style.transitionProperty  = new StyleList<StylePropertyName>(properties2);
-        _quitButton.style.transitionDuration = new StyleList<TimeValue>(durations2);
-        _quitButton.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
-        _quitButton.style.display = DisplayStyle.Flex;
+            List<EasingFunction> easingFunctions = new List<EasingFunction>();
+            easingFunctions.Add(new EasingFunction(EasingMode.Ease));
+            
+            _creditsButton.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
+            _creditsButton.style.opacity = 1.0f;
+            _creditsButton.style.visibility = new StyleEnum<Visibility>(Visibility.Visible);
+            _creditsButton.style.scale = new StyleScale(new Vector2(1.0f, 1.0f));
+            _creditsButton.style.width = new StyleLength(new Length(20.0f, LengthUnit.Percent));
+            _creditsButton.style.height = new StyleLength(new Length(12.0f, LengthUnit.Percent));
+            
+            _creditsXButton.style.display = DisplayStyle.None;
+            SetVisibility(DisplayStyle.Flex, new VisualElement[]{_title, _playButton, _optionsButton, _quitButton});
+        }
     }
 
     private void OnQuitButtonClicked()
