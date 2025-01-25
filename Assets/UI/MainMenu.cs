efbbe82732c3318f4090fbd98640906f20e59a4f
Wolfgang Groetz <wolfganggroetz@gmail.com>
2025-01-25 13:48:09 -0500

    Bubble button pop animation

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
+    public float popAnimationDuration = 0.1f;
+    
     private VisualElement _ui;
 
     private Button _playButton;
@@ -33,21 +36,63 @@ public class MainMenu : MonoBehaviour
     private void OnPlayButtonClicked()
     {
         Debug.Log("Play button clicked");
-        _ui.visible = false;
+        PlayButtonPopAnimation(_playButton);
     }
     
     private void OnOptionsButtonClicked()
     {
         Debug.Log("Options Button clicked");
+        ButtonPopAnimation(_optionsButton);
+    }
+
+    private void ButtonPopAnimation(Button button)
+    {
+        List<StylePropertyName> properties = new List<StylePropertyName>();
+        properties.Add(new StylePropertyName("scale"));
+        properties.Add(new StylePropertyName("opacity"));
+        properties.Add(new StylePropertyName("visibility"));
+        button.style.transitionProperty = new StyleList<StylePropertyName>(properties);
+        
+        List<TimeValue> durations = new List<TimeValue>();
+        durations.Add(new TimeValue(popAnimationDuration, TimeUnit.Second));
+        button.style.transitionDuration = new StyleList<TimeValue>(durations);
+        
+        List<EasingFunction> easingFunctions = new List<EasingFunction>();
+        easingFunctions.Add(new EasingFunction(EasingMode.EaseOut));
+        button.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
+        
+        button.style.scale = new StyleScale(new Vector2(1.3f, 1.3f));
+        button.style.opacity = 0;
+        button.style.visibility = new StyleEnum<Visibility>(Visibility.Hidden);
+    }
+
+    private void PlayButtonPopAnimation(Button button)
+    {
+        List<StylePropertyName> properties = new List<StylePropertyName>();
+        properties.Add(new StylePropertyName("visibility"));
+        _ui.style.transitionProperty = new StyleList<StylePropertyName>(properties);
+        
+        List<TimeValue> durations = new List<TimeValue>();
+        durations.Add(new TimeValue(popAnimationDuration, TimeUnit.Second));
+        _ui.style.transitionDuration = new StyleList<TimeValue>(durations);
+        
+        List<EasingFunction> easingFunctions = new List<EasingFunction>();
+        easingFunctions.Add(new EasingFunction(EasingMode.EaseOut));
+        button.style.transitionTimingFunction = new StyleList<EasingFunction>(easingFunctions);
+        _ui.style.visibility = new StyleEnum<Visibility>(Visibility.Hidden);
+        
+        ButtonPopAnimation(button);
     }
 
     private void OnCreditsButtonClicked()
     {
         Debug.Log("Credits Button clicked");
+        ButtonPopAnimation(_creditsButton);
     }
     
     private void OnQuitButtonClicked()
     {
+        ButtonPopAnimation(_quitButton);
         Application.Quit();
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
